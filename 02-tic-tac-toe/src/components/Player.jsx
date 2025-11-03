17113f46f40b7ad6054c5b303210f9f78b59757c
farshid-azady <mcpd.developer@gmail.com>
2025-11-02 20:04:22 -0800

    name change is activated

--- 02-tic-tac-toe/src/components/Player.jsx
+++ 02-tic-tac-toe/src/components/Player.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 
-const Player = ({ name, symbol }) => {
-    const[isEditing,setIsEditing]=useState(false);
-    function handleEditClick(){
-        setIsEditing(true);
-    }
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  function handleEditClick() {
+    setIsEditing((edited) => !edited);
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let playerContent = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    playerContent = (
+      <input required type="text" value={playerName} onChange={handleChange} />
+    );
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerContent}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
