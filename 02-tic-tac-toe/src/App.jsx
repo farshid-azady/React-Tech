f66366389525bbe052552b94adc40add11aa3c0b
farshid-azady <mcpd.developer@gmail.com>
2025-11-04 09:57:52 +0330

    activation style for p;ayer who is active by boarder line color

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  }
   return (
     <>
       <main>
         <div id="game-container">
-          <ol id="players">
+          <ol id="players" className="">
             <Player initialName="player 1" symbol="X" />
             <Player initialName="player 2" symbol="O" />
+            
           </ol>
-          <GameBoard />
+          <GameBoard onSelectSquare={handleSelectSquare}/>
         </div>
         LOG
       </main>
--- 02-tic-tac-toe/src/components/GameBoard.jsx
+++ 02-tic-tac-toe/src/components/GameBoard.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-const GameBoard = () => {
+const GameBoard = ({onSelectSquare}) => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleClick(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+        const updatedGameBoard = [...prevGameBoard.map(innerrArr => [...innerrArr])];
+        updatedGameBoard[rowIndex][colIndex] = "X";
+        return updatedGameBoard;
+      
+    });
+    onSelectSquare();
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((col, colIndex) => (
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{col}</button>
+                <button onClick={()=>handleClick(rowIndex, colIndex )}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
